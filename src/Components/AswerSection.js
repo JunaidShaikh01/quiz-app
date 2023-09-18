25c4120311f29bcaa3d36c028ba0448f620314f8
JunaidShaikh01 <shaikhjunaid020801@gmail.com>
2023-09-18 16:08:33 +0530

    Add Files

--- src/Components/AswerSection.js
+++ src/Components/AswerSection.js
@@ -1,113 +1,60 @@
-// import React from "react";
-// import { useReducer } from "react";
-// import QuestionSection from "./QuestionSection";
-
-// const initial = {
-//   currentQuestionIndex: 0,
-//   score: 0,
-// };
-// function reducer(state, action) {
-//   if (action.type === "NEXT_QUESTION") {
-//     return { ...state, currentQuestionIndex: state.currentQuestionIndex + 1 };
-//   }
-//   if (action.type === "CURRECT_ANSWER") {
-//     return {
-//       ...state,
-//       score: state.score + 1,
-//     };
-//   }
-// }
-// export default function AswerSection() {
-//   const [state, dispatch] = useReducer(reducer, initial);
-//   const currentQuestion = QuestionSection[state.currentQuestionIndex];
-//   function handleAnswerClick(selectedOption) {
-//     if (selectedOption === currentQuestion.correctAnswer) {
-//       dispatch({ type: "CURRECT_ANSWER" });
-//     }
-//     dispatch({ type: "NEXT_QUESTION" });
-//   }
-//   return (
-//     <div>
-//       {currentQuestion ? (
-//         <div>
-//           <h2>{currentQuestion.question}</h2>
-//           <ul>
-//             {currentQuestion.options.map((option, index) => (
-//               <li key={index} onClick={() => handleAnswerClick(option)}>
-//                 {option}
-//               </li>
-//             ))}
-//           </ul>
-//         </div>
-//       ) : (
-//         <div>
-//           <h2>Quiz Completed</h2>
-//           <p>Your Score {state.score}</p>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// Quiz.js
 import React, { useReducer } from "react";
 
 import { quizData } from "./quizData";
 
-const initialState = {
+const initial = {
   currentQuestionIndex: 0,
   score: 0,
 };
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "NEXT_QUESTION":
-      return {
-        ...state,
-        currentQuestionIndex: state.currentQuestionIndex + 1,
-      };
-    case "CORRECT_ANSWER":
-      return {
-        ...state,
-        score: state.score + 1,
-      };
-    default:
-      return state;
+function reducer(state, action) {
+  if (action.type === "CURRECT_ANSWER") {
+    return {
+      ...state,
+      score: state.score + 1,
+    };
   }
-};
+  if (action.type === "NEXT_QUESTION") {
+    return {
+      ...state,
+      currentQuestionIndex: state.currentQuestionIndex + 1,
+    };
+  }
+}
 
-const AswerSection = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+function AswerSection() {
+  const [state, dispatch] = useReducer(reducer, initial);
   const currentQuestion = quizData[state.currentQuestionIndex];
 
-  const handleAnswerClick = (selectedOption) => {
+  function handleAnswerClick(selectedOption) {
     if (selectedOption === currentQuestion.correctAnswer) {
-      dispatch({ type: "CORRECT_ANSWER" });
+      dispatch({ type: "CURRECT_ANSWER" });
     }
     dispatch({ type: "NEXT_QUESTION" });
-  };
-
+  }
   return (
     <div>
       {currentQuestion ? (
         <div>
           <h2>{currentQuestion.question}</h2>
-          <ul>
-            {currentQuestion.options.map((option, index) => (
-              <li key={index} onClick={() => handleAnswerClick(option)}>
-                {option}
-              </li>
-            ))}
-          </ul>
+          <div>
+            <ul>
+              {currentQuestion.options.map((options, index) => (
+                <li kay={index} onClick={() => handleAnswerClick(options)}>
+                  {options}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       ) : (
         <div>
-          <h2>Quiz Completed</h2>
-          <p>Your Score: {state.score}</p>
+          <h2>Game Over</h2>
+          <p>Your score : {state.score}</p>
         </div>
       )}
     </div>
   );
-};
+}
 
 export default AswerSection;
--- src/Components/quizData.js
+++ src/Components/quizData.js
@@ -11,14 +11,51 @@ export const quizData = [
     correctAnswer: "Jupiter",
   },
   {
-    question: "What is the largest planet in our solar system?",
-    options: ["Earth", "Mars", "Jupiter", "Venus"],
-    correctAnswer: "Jupiter",
+    question: "Who wrote the play 'Romeo and Juliet'?",
+    options: [
+      "Charles Dickens",
+      "William Shakespeare",
+      "Jane Austen",
+      "F. Scott Fitzgerald",
+    ],
+    correctAnswer: "William Shakespeare",
   },
   {
-    question: "What is the largest planet in our solar system?",
-    options: ["Earth", "Mars", "Jupiter", "Venus"],
-    correctAnswer: "Jupiter",
+    question: "What is the largest mammal in the world?",
+    options: ["Elephant", "Blue Whale", "Giraffe", "Hippopotamus"],
+    correctAnswer: "Blue Whale",
   },
+  {
+    question:
+      "Which gas do plants absorb from the atmosphere during photosynthesis?",
+    options: ["Oxygen", "Carbon Dioxide", "Nitrogen", "Hydrogen"],
+    correctAnswer: "Carbon Dioxide",
+  },
+  {
+    question: "Which continent is known as the 'Land Down Under'?",
+    options: ["North America", "Europe", "Australia", "Asia"],
+    correctAnswer: "Australia",
+  },
+  {
+    question: "Who is the author of 'To Kill a Mockingbird'?",
+    options: ["George Orwell", "Mark Twain", "Harper Lee", "J.K. Rowling"],
+    correctAnswer: "Harper Lee",
+  },
+  {
+    question: "What is the chemical symbol for gold?",
+    options: ["Go", "Ag", "Au", "Ag"],
+    correctAnswer: "Au",
+  },
+  {
+    question: "Which gas makes up the majority of Earth's atmosphere?",
+    options: ["Oxygen", "Nitrogen", "Carbon Dioxide", "Hydrogen"],
+    correctAnswer: "Nitrogen",
+  },
+  {
+    question: "Who is credited with inventing the World Wide Web (WWW)?",
+    options: ["Bill Gates", "Tim Berners-Lee", "Steve Jobs", "Elon Musk"],
+    correctAnswer: "Tim Berners-Lee",
+  },
+
   // Add more questions here
 ];

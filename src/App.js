23c89a14f6f8efd00daed1a42b2f105c895ac0f2
JunaidShaikh01 <shaikhjunaid020801@gmail.com>
2023-09-18 15:36:43 +0530

    Add all files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import AswerSection from "./Components/AswerSection";
+
+function App() {
+  return (
+    <div className="App">
+      <AswerSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/quizData.js
@@ -0,0 +1,24 @@
+// quizData.js
+export const quizData = [
+  {
+    question: "What is the capital of France?",
+    options: ["Madrid", "Berlin", "Paris", "Rome"],
+    correctAnswer: "Paris",
+  },
+  {
+    question: "What is the largest planet in our solar system?",
+    options: ["Earth", "Mars", "Jupiter", "Venus"],
+    correctAnswer: "Jupiter",
+  },
+  {
+    question: "What is the largest planet in our solar system?",
+    options: ["Earth", "Mars", "Jupiter", "Venus"],
+    correctAnswer: "Jupiter",
+  },
+  {
+    question: "What is the largest planet in our solar system?",
+    options: ["Earth", "Mars", "Jupiter", "Venus"],
+    correctAnswer: "Jupiter",
+  },
+  // Add more questions here
+];
